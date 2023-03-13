3b87a64c920c9868d09f9eeac670aa5747adaa89
harshal <harshal.jain.csai.20@ggits.net>
2023-03-13 20:54:05 +0530

    day6

--- src/PAGES/Product/ProductPage.js
+++ src/PAGES/Product/ProductPage.js
@@ -3,6 +3,8 @@ import { Link, useParams } from 'react-router-dom'
 import img1 from '../../ASSETS/Images/1.png'
 import img2 from '../../ASSETS/Images/2.png'
 import img3 from '../../ASSETS/Images/3.png'
+import Footer1 from '../../COMPONENTS/Footer/Footer1'
+import Footer2 from '../../COMPONENTS/Footer/Footer2'
 import Navbar from '../../COMPONENTS/Navbar/Navbar'
 import './ProductPage.css'
 const ProductPage = () => {
@@ -11,7 +13,7 @@ const ProductPage = () => {
     const [productdata, setproductdata] = React.useState([])
     const [activeimg, setactiveimg] = React.useState({})
     const [count, setcount] = React.useState(1)
-
+    const [showreview, setshowreview] = React.useState(false)
 
     const getproductdatabyid = async () => {
         let temp = {
@@ -21,7 +23,7 @@ const ProductPage = () => {
                 {
                     "ProductId": 1,
                     "ProductName": "Product 1",
-                    "ProductDescription": "Product 1 Description",
+                    "ProductDescription": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                     "ProductImage": [
                         {
                             id: 1,
@@ -61,7 +63,33 @@ const ProductPage = () => {
                     "ProductShippingReturnPolicy": "Return Policy 1",
                     "ProductShippingReturnPolicyType": "Days",
                     "ProductShippingReturnPolicyDescription": "Return Policy Description 1",
-                    "ProductShippingReturnPolicyDescriptionType": "Days"
+                    "ProductShippingReturnPolicyDescriptionType": "Days",
+                    "ProductReviews": [
+                        {
+                            "ReviewId": 1,
+                            "Name": "Harshal Jain",
+                            "Email": "",
+                            "Rating": 5,
+                            "Date": "2021-08-01",
+                            "Review": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                        },
+                        {
+                            "ReviewId": 2,
+                            "Name": "Viraj",
+                            "Email": "",
+                            "Rating": 1,
+                            "Date": "2021-08-01",
+                            "Review": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+                        },
+                        {
+                            "ReviewId": 3,
+                            "Name": "Harshal Jain",
+                            "Email": "",
+                            "Rating": 4,
+                            "Date": "2021-08-01",
+                            "Review": "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                        }
+                    ]
                 }
             ]
         }
@@ -100,17 +128,17 @@ const ProductPage = () => {
                 <div className='c11'>
                     <div className='imgset'>
                         {
-                            imageset && imageset?.map((item,index)=>{
+                            imageset && imageset?.map((item, index) => {
                                 return (
                                     <div className='imgsmall'
-                                        onClick={()=>{
+                                        onClick={() => {
                                             setactiveimg(item)
                                         }}
                                     >
-                                        <img src={item.image} alt="" 
-                                         className={
-                                            activeimg.id == item.id ? 'active' : ''
-                                         }
+                                        <img src={item.image} alt=""
+                                            className={
+                                                activeimg.id == item.id ? 'active' : ''
+                                            }
                                         />
                                     </div>
                                 )
@@ -167,6 +195,103 @@ const ProductPage = () => {
                     </div>
                 </div>
             </div>
+            <div className='pc2'>
+                {
+                    showreview ?
+                        <div className='tabs'>
+                            <button
+                                className='inactive'
+
+                                onClick={
+                                    () => {
+                                        setshowreview(false)
+                                    }
+                                }
+                            >Description</button>
+                            <button
+                                className='active'
+                                onClick={
+                                    () => {
+                                        setshowreview(true)
+                                    }
+                                }
+                            >Reviews</button>
+                        </div>
+                        :
+                        <div className='tabs'>
+                            <button
+                                className='active'
+                                onClick={
+                                    () => {
+                                        setshowreview(false)
+                                    }
+                                }
+                            >
+                                Description
+                            </button>
+
+                            <button
+                                className='inactive'
+
+                                onClick={
+                                    () => {
+                                        setshowreview(true)
+                                    }
+                                }
+                            >Reviews</button>
+                        </div>
+                }
+                {
+                    showreview ?
+                        <div className='reviewcont'>
+                            <form>
+                                <div className='fromgroup'>
+                                    <label htmlFor="">Name</label>
+                                    <input type="text" />
+                                </div>
+
+                                <div className='fromgroup'>
+                                    <label htmlFor="">Email</label>
+                                    <input type="email" />
+                                </div>
+
+                                <div className='fromgroup'>
+                                    <label htmlFor="">Review</label>
+                                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                                </div>
+
+                                <button>Submit</button>
+                            </form>
+
+
+                            <div className='allreview'>
+                                <h1 className='head1'>Product Reviews</h1>
+                                {productdata.ProductReviews &&
+                                    productdata.ProductReviews.map((item, index) => {
+                                        return (
+                                            <div className='review'>
+                                                <div className='reviewhead'>
+                                                    <p className='name'>{item.Name}</p>
+                                                    <span className='date'>{item.Date}</span>
+                                                </div>
+
+                                                <div className='reviewbody'>
+                                                    {item.Review}
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
+                        :
+                        <p className='desc'>
+                            {productdata.ProductDescription}
+                        </p>
+                }
+            </div>
+            <Footer1 />
+            <Footer2 />
         </div>
     )
 }
